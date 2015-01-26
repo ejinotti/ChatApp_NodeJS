238944495c9337f3f8cacfb0016d863e5554ce0a
Edward J. Jinotti <ejinotti2@gmail.com>
2015-01-26 15:30:46 -0500

    In the middle of major re-design, but having to change focus to other projects so everything broken..

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -15,11 +15,19 @@ Chat.prototype.processCommand = function (cmd) {
     if (nick.match(/^[\w|\d]+$/)) {
       this.socket.emit('nicknameChangeRequest', nick);
     } else {
-      console.log('invalid nick');
       throw "Invalid Nickname!";
     }
+  } else if ((matchData = cmd.match(/^\/join\s+(.*)/))) {
+    var room = matchData[1];
+
+    if (room.match(/^[\w|\d]+$/)) {
+      this.socket.emit('joinRoomRequest', room);
+    } else {
+      throw "Invalid Room!";
+    }
+  } else if (cmd.match(/^\/leave\s+/)) {
+    this.socket.emit("leaveRoomRequest");
   } else {
-    console.log('invalid command');
-    throw "Invalid command!";
+    throw "Invalid Command!";
   }
 };
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,58 +1,152 @@
+function ChatUI (socket) {
+  this.socket = socket;
+  this.rooms = {};
 
-$(document).ready(function () {
-  var socket = io();
-  var chat = new Chat(socket);
+  this.socket.on("message", this.handleMessage);
+  this.socket.on("nicknameChangeResult", this.handleNickChange);
+  this.socket.on("joinRoomResult", this.handleJoinRoom);
+  this.socket.on("roomList", this.handleRoomList);
 
-  // display msgs from server.
-  socket.on('message', function (data) {
+  $("#sendmsg").on("submit", this.processUserInput);
+}
 
+ChatUI.prototype.handleMessage = function (data) {
+  var $li = $('<li>');
+
+  if (data.bold) {
+    $li.html('<b>' + _.escape(data.message) + '</b>');
+  } else {
+    $li.html(_.escape(data.message));
+  }
+
+  $('#chat').find('ul').append($li);
+};
+
+ChatUI.prototype.handleNickChange = function (data) {
+  if (data.success) {
+    $('.nick').html(_.escape(data.message));
+  } else {
     var $li = $('<li>');
-
-    if (data.bold) {
-      $li.html('<b>' + _.escape(data.message) + '</b>');
-    } else {
-      $li.html(_.escape(data.message));
-    }
-
+    $li.html('<b>' + _.escape(data.message) + '</b>');
     $('#chat').find('ul').append($li);
-  });
+  }
+};
 
-  // handle nick change response
-  socket.on('nicknameChangeResult', function (data) {
-    if (data.success) {
-      $('.nick').html(_.escape(data.message));
-    } else {
-      var $li = $('<li>');
-      $li.html('<b>' + _.escape(data.message) + '</b>');
-      $('#chat').find('ul').append($li);
-    }
-  });
+ChatUI.prototype.handleJoinRoom = function (data) {
 
-  // handle user input
-  $('#sendmsg').on('submit', function (event) {
-    event.preventDefault();
+};
 
-    var $inputEl = $(this).find('input');
+ChatUI.prototype.handleRoomList = function (data) {
 
-    var input = $inputEl.val();
+};
 
-    if (input[0] === "/") {
-      try {
-        chat.processCommand(input);
-      } catch (err) {
-        if (typeof err === "string") {
-          var $li = $('<li>');
-          $li.html('<b>' + err + '</b>');
-          $('#chat').find('ul').append($li);
-        } else {
-          throw err;
-        }
+ChatUI.prototype.processUserInput = function (event) {
+  event.preventDefault();
+
+  var $inputEl = $(this).find('input');
+
+  var input = $inputEl.val();
+
+  if (input[0] === "/") {
+    try {
+      chat.processCommand(input);
+    } catch (err) {
+      if (typeof err === "string") {
+        var $li = $('<li>');
+        $li.html('<b>' + err + '</b>');
+        $('#chat').find('ul').append($li);
+      } else {
+        throw err;
       }
-    } else if (input.length > 0){
-      chat.sendMessage(input);
     }
+  } else if (input.length > 0){
+    chat.sendMessage(input);
+  }
 
-    $inputEl.val("");
-  });
+  $inputEl.val("");
+};
 
-});
+ChatUI.prototype.processCommand = function (cmd) {
+  var matchData = [];
+
+  if ((matchData = cmd.match(/^\/nick\s+(.*)/))) {
+    var nick = matchData[1];
+
+    if (nick.match(/^[a-zA-Z]\w*$/)) {
+      this.socket.emit('nicknameChangeRequest', nick);
+    } else {
+      throw "Invalid Nickname!";
+    }
+  } else if ((matchData = cmd.match(/^\/join\s+(.*)/))) {
+    var room = matchData[1];
+
+    if (room.match(/^[\w|\d]+$/)) {
+      this.socket.emit('joinRoomRequest', room);
+    } else {
+      throw "Invalid Room!";
+    }
+  } else if (cmd.match(/^\/leave\s+/)) {
+    this.socket.emit("leaveRoomRequest");
+  } else {
+    throw "Invalid Command!";
+  }
+};
+
+//
+// $(document).ready(function () {
+//   var socket = io();
+//   var chat = new Chat(socket);
+//
+//   // display msgs from server.
+//   socket.on('message', function (data) {
+//
+//     var $li = $('<li>');
+//
+//     if (data.bold) {
+//       $li.html('<b>' + _.escape(data.message) + '</b>');
+//     } else {
+//       $li.html(_.escape(data.message));
+//     }
+//
+//     $('#chat').find('ul').append($li);
+//   });
+//
+//   // handle nick change response
+//   socket.on('nicknameChangeResult', function (data) {
+//     if (data.success) {
+//       $('.nick').html(_.escape(data.message));
+//     } else {
+//       var $li = $('<li>');
+//       $li.html('<b>' + _.escape(data.message) + '</b>');
+//       $('#chat').find('ul').append($li);
+//     }
+//   });
+//
+//   // handle user input
+//   $('#sendmsg').on('submit', function (event) {
+//     event.preventDefault();
+//
+//     var $inputEl = $(this).find('input');
+//
+//     var input = $inputEl.val();
+//
+//     if (input[0] === "/") {
+//       try {
+//         chat.processCommand(input);
+//       } catch (err) {
+//         if (typeof err === "string") {
+//           var $li = $('<li>');
+//           $li.html('<b>' + err + '</b>');
+//           $('#chat').find('ul').append($li);
+//         } else {
+//           throw err;
+//         }
+//       }
+//     } else if (input.length > 0){
+//       chat.sendMessage(input);
+//     }
+//
+//     $inputEl.val("");
+//   });
+//
+// });
